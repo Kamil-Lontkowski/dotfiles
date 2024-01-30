7eba2d5ccefb51bac0be4542b697071276152d14
Kamil-Lontkowski <kamillont14@gmail.com>
2024-01-30 20:37:10 +0100

    fzf colors

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -34,6 +34,12 @@ Get-ChildItem -Path $UserAutoCompletions -Filter *.ps1 | ForEach-Object {
     . $_.FullName
 }
 
+$ENV:FZF_DEFAULT_OPTS=@"
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
+"@
+
 function quick { Set-Location 'C:\Users\werbu\OneDrive\quicknotes' }
 
 function cleanScala {
